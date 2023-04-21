7163ca28b11819f61430924274b3c9b193e51a58
Dhruv_Rana <ranadhruv42@gmail.com>
2023-04-21 15:16:22 -0400

    Adding AWS Lambda source code using Java SDK for SNS Topic based Email Notification support.

---
+++ lambda-func-java-sdk/src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
@@ -0,0 +1,38 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class LambdaFunctionHandlerTest {
+
+    private static final String SAMPLE_INPUT_STRING = "{\n"
+    		+ "    \"notificationMsg\":\"test3\",\n"
+    		+ "    \"notificationSubscribers\":\"ranadhruv42@gmail.com,ranaavani712@gmail.com,dhruvrana4244@gmail.com\"\n"
+    		+ "}";
+    
+    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
+
+    @Test
+    public void testLambdaFunctionHandler() throws IOException {
+        LambdaFunctionHandler handler = new LambdaFunctionHandler();
+
+        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+
+        // TODO: validate output here if needed.
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        Assert.assertNotEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+    }
+}
