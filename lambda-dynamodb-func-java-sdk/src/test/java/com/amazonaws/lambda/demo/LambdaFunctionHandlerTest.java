5a220ffbbd6ab7f91a0914dbf40618c48d5b480e
Dhruv_Rana <ranadhruv42@gmail.com>
2023-04-25 01:30:56 -0400

    Adding AWS Lambda with Java SDK support for DynamoDB Ops.

---
+++ lambda-dynamodb-func-java-sdk/src/test/java/com/amazonaws/lambda/demo/LambdaFunctionHandlerTest.java
@@ -0,0 +1,39 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class LambdaFunctionHandlerTest {
+
+    private static final String SAMPLE_INPUT_STRING = "{\n"
+    		+ "    \"id\":\"ioufhvs-id\",\n"
+    		+ "    \"firstName\":\"test-firstName\",\n"
+    		+ "    \"lastName\":\"test-lastName\"\n"
+    		+ "}";
+    
+    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
+
+    @Test
+    public void testLambdaFunctionHandler() throws IOException {
+        LambdaFunctionHandler handler = new LambdaFunctionHandler();
+
+        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+
+        // TODO: validate output here if needed.
+        String sampleOutputString = output.toString();
+        System.out.println(sampleOutputString);
+        Assert.assertNotEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+    }
+}
